3cc7e79018275d53f860309f11bae3d1baa4d866
MiranaSGit <omerfarukdemirozu@gmail.com>
2022-01-31 09:46:07 +0300

    commit

--- FlaskFiles/image_for_result_server/app.py
+++ FlaskFiles/image_for_result_server/app.py
@@ -13,23 +13,26 @@ app.config['MYSQL_DATABASE_DB'] = os.getenv('MYSQL_DATABASE')
 app.config['MYSQL_DATABASE_USER'] = os.getenv('MYSQL_USER')
 #app.config['MYSQL_DATABASE_PORT'] = 3306
 mysql = MySQL()
-mysql.init_app(app) 
+mysql.init_app(app)
 connection = mysql.connect()
 connection.autocommit(True)
 cursor = connection.cursor()
 
 # Write a function named `find_persons` which finds persons' record using the keyword from the phonebook table in the db,
-# and returns result as list of dictionary 
+# and returns result as list of dictionary
 # `[{'id': 1, 'name':'XXXX', 'number': 'XXXXXX'}]`.
+
+
 def find_persons(keyword):
     query = f"""
     SELECT * FROM phonebook WHERE name like '%{keyword.strip().lower()}%';
     """
     cursor.execute(query)
     result = cursor.fetchall()
-    persons =[{'id':row[0], 'name':row[1].strip().title(), 'number':row[2]} for row in result]
+    persons = [{'id': row[0], 'name':row[1].strip().title(), 'number':row[2]}
+               for row in result]
     if len(persons) == 0:
-        persons = [{'name':'No Result', 'number':'No Result'}]
+        persons = [{'name': 'No Result', 'number': 'No Result'}]
     return persons
 
 
@@ -41,12 +44,12 @@ def find_records():
     if request.method == 'POST':
         keyword = request.form['username']
         persons = find_persons(keyword)
-        return render_template('index.html', persons=persons, keyword=keyword, show_result=True, developer_name='Eyup')
+        return render_template('index.html', persons=persons, keyword=keyword, show_result=True, developer_name='OFD')
     else:
-        return render_template('index.html', show_result=False, developer_name='Eyup')
+        return render_template('index.html', show_result=False, developer_name='OFD')
 
 
 # Add a statement to run the Flask application which can be reached from any host on port 80.
-if __name__== '__main__':
-       #app.run(debug=True)
-    app.run(host='0.0.0.0', port=80) 
+if __name__ == '__main__':
+    # app.run(debug=True)
+    app.run(host='0.0.0.0', port=80)
